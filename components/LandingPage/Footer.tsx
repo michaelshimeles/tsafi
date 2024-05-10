91f2a4c8f2997b970ddeaf83ee7ea17230b10158
Michael Shimeles <michaelwasihun96@gmail.com>
2024-05-10 15:38:21 -0400

    Clean up

--- components/LandingPage/Footer.tsx
+++ components/LandingPage/Footer.tsx
@@ -3,31 +3,21 @@ import Link from 'next/link'
 
 const navigation = {
     connect: [
-        { name: 'Book Meeting', href: '' },
-        {
-            name: 'Twitter',
-            href: 'https://twitter.com/justansub',
-        },
         {
             name: 'Github',
-            href: 'https://www.youtube.com/@SpeedyBrand-SEO',
-        },
-        {
-            name: 'LinkedIn',
-            href: 'https://www.linkedin.com/company/speedy-brand-inc/',
+            href: 'https://github.com/michaelshimeles/cms',
         },
     ],
     company: [
-        { name: 'Blogs', href: '/' },
-        { name: 'Pricing', href: '/' },
-        { name: 'Affiliate Partner', href: '/' },
-        { name: 'AI For Enterprise', href: '/' },
+        { name: 'Twitter', href: 'https://x.com/rasmickyy' },
+        { name: 'YouTube', href: 'https://www.youtube.com/@rasmic' },
+        { name: 'Discord', href: 'https://discord.gg/TRccaehh8n' },
     ],
 }
 
 const Footer = () => {
     return (
-        <main className="flex min-w-screen flex-col items-center justify-between px-7 pb-7">
+        <main className="flex min-w-screen flex-col items-center justify-between border-t pt-[4rem] px-7 pb-7">
             <footer
                 aria-labelledby="footer-heading"
                 className="font-inter w-full max-w-7xl"
@@ -40,7 +30,7 @@ const Footer = () => {
                         <div className="space-y-8">
                             <p className='font-semibold'>SupaNext CMS</p>
                             <p className="text-md max-w-xs leading-6 text-gray-700">
-                                Not your average component library - build faster, launch sooner.
+                                I built this because I didn&apos;t want to read the docs of other CMS.
                             </p>
                             <div className="flex space-x-6 text-sm text-gray-700">
                                 <div>Made with ❤️ by Micky.</div>
@@ -50,7 +40,7 @@ const Footer = () => {
                         <div className="mt-16 grid grid-cols-2 gap-14 md:grid-cols-2 lg:mt-0 xl:col-span-2">
                             <div className="md:mt-0">
                                 <h3 className="text-sm font-semibold leading-6 text-gray-900">
-                                    Connect
+                                    Code
                                 </h3>
                                 <div className="mt-6 space-y-4">
                                     {navigation.connect.map((item) => (
@@ -70,7 +60,7 @@ const Footer = () => {
                             <div>
                                 <div>
                                     <h3 className="text-sm font-semibold leading-6 text-gray-900">
-                                        Company
+                                        Social
                                     </h3>
                                     <div className="mt-6 space-y-4">
                                         {navigation.company.map((item) => (
@@ -90,7 +80,7 @@ const Footer = () => {
                     </div>
                     <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
                         <p className="text-xs leading-5 text-gray-700">
-                            &copy; 2024 SyntaxUI. All rights reserved.
+                            &copy; 2024 SupaNext CMS. All rights reserved.
                         </p>
                     </div>
                 </div>
