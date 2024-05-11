bb0c42705a355e9567cef2b53a5170433322ad0f
Michael Shimeles <michaelwasihun96@gmail.com>
2024-05-11 13:54:59 -0400

    Added some glitter to the homepage

--- app/cms/settings/page.tsx
+++ app/cms/settings/page.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
 import UserInfo from './(components)/UserInfo'
-import BlogProxy from './(components)/BlogProxy'
 
 export default function SettingsPage() {
   return (
     <div className="flex flex-col justify-center items-start w-full mt-2 pb-2">
       <UserInfo />
-      {/* <BlogProxy /> */}
     </div>
   )
 }
