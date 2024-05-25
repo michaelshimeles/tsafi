5b91616f17c5310eaebf8a997adf2a79575f4833
Michael Shimeles <michaelwasihun96@gmail.com>
2024-05-25 18:08:59 -0400

    Clean dashboard

--- app/cms/(components)/DashboardSidebar.tsx
+++ app/cms/(components)/DashboardSidebar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { Separator } from '@radix-ui/react-dropdown-menu'
+import { Separator } from '@/components/ui/separator'
 import clsx from 'clsx'
 import {
   BookA,
@@ -82,7 +82,7 @@ export default function DashboardNav() {
               </div>
               Create Category
             </Link>
-            <Separator className="my-3" />
+            <Separator className="my-[0.75rem]" />
             <Link
               className={clsx("flex items-center gap-2 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50", {
                 "flex items-center gap-2 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50": pathname === "/cms/api"
