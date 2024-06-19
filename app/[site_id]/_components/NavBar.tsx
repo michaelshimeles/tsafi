046e15e29f68cfda0d0f1bfdca863764f9fae396
Michael Shimeles <michaelwasihun96@gmail.com>
2024-06-19 18:40:57 -0400

    Fresh navbar

--- app/[site_id]/_components/NavBar.tsx
+++ app/[site_id]/_components/NavBar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ModeToggle } from "@/components/ModeToggle"
+import ModeToggle from "@/components/ModeToggle"
 import { Button } from "@/components/ui/button"
 import {
   NavigationMenu,
