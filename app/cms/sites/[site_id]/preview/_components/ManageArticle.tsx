1b4f2eb9f6d368d6ee5dd51e427b1476e9c74a34
Michael Shimeles <michaelwasihun96@gmail.com>
2024-06-28 10:12:37 -0400

    Remove preview and shareability

--- app/cms/sites/[site_id]/preview/_components/ManageArticle.tsx
+++ app/cms/sites/[site_id]/preview/_components/ManageArticle.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from 'react'
+import { Button } from '@/components/ui/button'
 import {
   Dialog,
   DialogContent,
@@ -8,26 +8,14 @@ import {
   DialogTitle,
   DialogTrigger
 } from "@/components/ui/dialog"
-import { statusBlogs } from '@/utils/actions/blog/status-publish-blog'
 import { deleteBlog } from '@/utils/actions/blog/delete-blog'
-import { useRouter } from 'next/navigation'
-import { ClipboardCheckIcon, Delete, DeleteIcon, Edit, Share, Trash } from 'lucide-react'
-import Link from 'next/link'
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { Separator } from '@/components/ui/separator';
-import { useForm } from 'react-hook-form'
+import { statusBlogs } from '@/utils/actions/blog/status-publish-blog'
 import { useGetArticleBySlug } from '@/utils/hooks/useGetArticleBySlug'
-import { toast } from 'sonner'
-import { shareArticle } from '@/utils/actions/articles/share-article'
 import { Article } from '@/utils/types'
+import { Edit, Trash } from 'lucide-react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 export default function ManageArticle({ params, response }: {
   params: {
@@ -43,67 +31,8 @@ export default function ManageArticle({ params, response }: {
 
   const { data, isPending, refetch } = useGetArticleBySlug(params?.slug);
 
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm()
-
-  const onSubmit = async (data: any) => {
-    const shareSetting = data.shareSetting === 'true'; // Convert back to boolean
-    try {
-      const response = await shareArticle(params?.slug, shareSetting)
-      toast("Article shareability changed")
-      refetch()
-      return response
-    } catch (error) {
-      console.log('error', error)
-      return error
-    }
-  };
-
-
   return (
     <div className='flex justify-end items-center w-full gap-2'>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button size="sm" variant="outline">
-            <Share className='w-4 h-4' />
-          </Button>
-        </PopoverTrigger>
-        {!isPending &&
-          <PopoverContent className="w-80 dark:bg-opacity-40 dark:bg-black">
-            <h4 className="font-medium leading-none">
-              Shareability
-            </h4>
-            <Separator className='w-full mt-3' />
-            <form onSubmit={handleSubmit(onSubmit)}>
-              {<RadioGroup defaultValue={data?.[0]?.shareable ? "true" : "false"} {...register("shareSetting")} className='flex flex-col gap-2 py-3'>
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="true" id="publicOption" />
-                  <Label>Public</Label>
-                </div>
-                <div className="flex items-center space-x-2">
-                  <RadioGroupItem value="false" id="privateOption" />
-                  <Label>Private</Label>
-                </div>
-              </RadioGroup>}
-              <Button type='submit' variant="outline">Update</Button>
-            </form>
-            <div className='flex gap-2 mt-4'>
-              <Input defaultValue={`${process.env.NEXT_PUBLIC_FRONTEND_URL}/article/public/${data?.[0]?.id}`} />
-              <Button size="icon" disabled={!data?.[0]?.shareable} onClick={() => {
-                navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/article/public/${data?.[0]?.id}`)
-                toast("Public article url has been copied")
-              }}>
-                <ClipboardCheckIcon />
-              </Button>
-            </div>
-          </PopoverContent>}
-
-      </Popover>
-
       <Link href={`/cms/sites/${params?.site_id}/preview/${params?.slug}/edit`}>
         <Button size="sm" variant="outline">
           <Edit className='w-4 h-4' />
