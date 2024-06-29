73322fddc1b905b91aaeb69e743e8654ab695177
Michael Shimeles <michaelwasihun96@gmail.com>
2024-06-29 11:17:23 -0400

    ai working

--- app/cms/ai/page.tsx
+++ app/cms/ai/page.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import AiComponent from './_components/ai'
 import { readMessages } from '@/utils/actions/ai/read-messages'
 import { auth } from '@clerk/nextjs/server';
+import Chat from './_components/chat';
+import { Message } from "ai";
+import { storeMessages } from '@/utils/functions/ai/store-messages';
 
 export default async function Ai() {
   const { userId } = auth();
 
   const response = await readMessages(userId!)
 
+  const messages: Message[] = response?.[0]?.messages
+
+  if (!messages) {
+    await storeMessages(userId!, []);
+  }
+
   return (
-    <AiComponent initialMessages={response?.[0]?.messages}/>
+    <Chat messages={messages?.map((m, i) => ({ ...m, id: i.toString() }))} />
   )
 }
