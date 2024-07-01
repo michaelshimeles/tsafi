e4a369949038b2e1f20af71d559889a0f6651e8e
Michael Shimeles <michaelwasihun96@gmail.com>
2024-07-01 12:30:18 -0400

    Minor bug fixes for ai

--- app/cms/ai/_components/chat.tsx
+++ app/cms/ai/_components/chat.tsx
@@ -32,21 +32,24 @@ export default function Chat({ messages: initialMessages }: { messages: Message[
     const message = toolInvocation?.result?.message;
     const result = toolInvocation?.result?.result;
 
+    console.log('toolInvocation', toolInvocation)
     switch (toolInvocation.toolName) {
       case 'create_site':
         return (
-          <div key={toolCallId} className='bg-blue-700 bg-opacity-10 text-sm whitespace-pre-wrap px-3 py-2 rounded-lg'>
-            {message}
+          <div>
+            <div key={toolCallId} className='bg-blue-700 bg-opacity-10 text-sm whitespace-pre-wrap px-3 py-2 rounded-lg w-fit'>
+              {message}
+            </div>
             <div className="pt-[0.5rem]">
-              {result && (
+              {toolInvocation?.result && (
                 <div className='flex gap-1'>
-                  <Link href={`https://${result.site_subdomain}.tsafi.xyz`} target="_blank">
+                  <Link href={`https://${toolInvocation?.result?.site_subdomain}.tsafi.xyz`} target="_blank">
                     <Button variant="outline" size="sm">
                       <ExternalLink className="mr-1 w-4 h-4" />
                       Website
                     </Button>
                   </Link>
-                  <Link href={`/cms`} target="_blank">
+                  <Link href={`/cms/sites/${toolInvocation?.result?.site_id}`} target="_blank">
                     <Button variant="outline" size="sm">
                       <ExternalLink className="mr-1 w-4 h-4" />
                       Dashboard
@@ -59,8 +62,10 @@ export default function Chat({ messages: initialMessages }: { messages: Message[
         );
       case 'read_sites':
         return (
-          <div key={toolCallId} className='flex gap-2 flex-col bg-blue-700 bg-opacity-10 text-sm whitespace-pre-wrap px-3 py-2 rounded-lg'>
-            <p>{message}</p>
+          <div>
+            <div key={toolCallId} className='flex gap-2 flex-col bg-blue-700 bg-opacity-10 text-sm whitespace-pre-wrap px-3 py-2 rounded-lg w-fit'>
+              <p>{message}</p>
+            </div>
             <div className='flex gap-2 flex-wrap'>
               {result?.length > 0 && result.map((site: any) => (
                 <Link key={site.site_id} href={`/cms/sites/${site.site_subdomain}`} prefetch={true} className="flex flex-col rounded-md w-[350px] hover:cursor-pointer transition-shadow duration-300" target='_blank'>
@@ -81,12 +86,12 @@ export default function Chat({ messages: initialMessages }: { messages: Message[
                 </Link>
               ))}
             </div>
-          </div>
+          </div >
         );
       case 'update_site_name':
       case 'update_sub_domain':
         return (
-          <div key={toolCallId} className='bg-blue-700 bg-opacity-10 text-sm whitespace-pre-wrap px-3 py-2 rounded-lg'>
+          <div key={toolCallId} className='bg-blue-700 bg-opacity-10 text-sm whitespace-pre-wrap px-3 py-2 rounded-lg w-fit'>
             {message && JSON.parse(result).map((info: any) => (
               <div key={info?.site_name}>
                 {message + " "}
@@ -105,18 +110,20 @@ export default function Chat({ messages: initialMessages }: { messages: Message[
       <div className="flex flex-col h-[calc(100vh-100px)] w-full">
         <div className="flex-grow overflow-y-auto p-4">
           {messages.map((m: any) => (
-            <div key={m.id} className={m.role === "user" ? "flex flex-col justify-center items-end w-full gap-1 mb-4" : "flex flex-col justify-center items-start w-full gap-1 mb-4"}>
+            <div key={m.id} className={m.role === "user" ? "flex flex-col justify-center items-end gap-1 mb-4" : "flex flex-col justify-center items-start gap-1 mb-4"}>
               <div className="flex flex-col gap-1 max-w-[80%]">
                 <p className="text-sm text-left">
                   {m.role.charAt(0).toUpperCase() + m.role.slice(1)}
                 </p>
                 {m?.toolInvocations?.map(renderToolInvocation)}
                 {m?.content && (
-                  <div className='bg-blue-700 whitespace-pre-wrap bg-opacity-10 text-sm px-3 py-2 rounded-lg'>
-                    {m?.content}
-                    <div className='flex gap-2 flex-wrap w-full'>
+                  <div>
+                    <div className='bg-blue-700 whitespace-pre-wrap bg-opacity-10 text-sm px-3 py-2 rounded-lg w-fit'>
+                      {m?.content}
+                    </div>
+                    <div className='flex gap-2 flex-wrap w-full mt-2'>
                       {m?.type === "tool-result_read-site" && m?.result?.map((info: any) => (
-                        <Link key={info.id} href={`/cms/sites/${info.site_subdomain}`} prefetch={true} className="flex flex-col rounded-md w-[350px] hover:cursor-pointer transition-shadow duration-300" target='_blank'>
+                        <Link key={info.id} href={`/cms/sites/${info.site_subdomain}`} prefetch={true} className="flex flex-col rounded-md max-w-[350px] w-full min-h-[150px] hover:cursor-pointer transition-shadow duration-300" target='_blank'>
                           <Card className="flex flex-col px-[1rem] justify-between h-full py-[1rem]">
                             <div className='flex flex-col w-full justify-center items-start'>
                               <h2 className="text-lg font-bold">{info.site_name}</h2>
@@ -134,7 +141,7 @@ export default function Chat({ messages: initialMessages }: { messages: Message[
                         </Link>
                       ))}
                       {m?.type === "tool-result_create-site" && ((
-                        <div className='flex gap-1'>
+                        <div className='flex gap-1 mt-1 '>
                           <Link href={`https://${m?.result?.site_subdomain}.tsafi.xyz`} target="_blank">
                             <Button variant="outline" size="sm">
                               <ExternalLink className="mr-1 w-4 h-4" />
@@ -161,6 +168,6 @@ export default function Chat({ messages: initialMessages }: { messages: Message[
           <PromptForm input={input} handleInputChange={handleInputChange} handleSubmit={handleSubmit} setInput={setInput} />
         </div>
       </div>
-    </DashWrapper>
+    </DashWrapper >
   );
 }
